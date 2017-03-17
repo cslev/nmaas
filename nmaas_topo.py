3185ec21678e960c38925f75d004536e13b5581c
cslev <cs.lev@gmx.com>
2017-03-16 19:41:34 -0700

    Rename nmaas_topo from nmass_topo and add 50% loss to link s3-s1

--- nmaas_topo.py
+++ nmaas_topo.py
@@ -21,11 +21,20 @@ class NMaaSTopo(Topo):
         h1 = self.addHost("h1", ip="10.0.0.1", mac="00:00:00:00:00:01")
         h2 = self.addHost("h2", ip="10.0.0.2", mac="00:00:00:00:00:02")
         h3 = self.addHost("h3", ip="10.0.0.3", mac="00:00:00:00:00:03")
+        h4 = self.addHost("h4", ip="10.0.0.4", mac="00:00:00:00:00:04")
+        h5 = self.addHost("h5", ip="10.0.0.5", mac="00:00:00:00:00:05")
 
         s1 = self.addSwitch("s1", protocols="OpenFlow13")
         s2 = self.addSwitch("s2", protocols="OpenFlow13")
+        s3 = self.addSwitch("s3", protocols="OpenFlow13")
+
 
         self.addLink(s1,h1,delay='10ms')
         self.addLink(s2, h2, delay='5ms')
         self.addLink(s2, h3, delay='1ms')
         self.addLink(s1, s2, delay='2ms')
+        self.addLink(s2, s3, delay='20ms')
+        self.addLink(s3, h4, delay='1ms')
+        self.addLink(s3, h5, delay='1ms')
+        self.addLink(s1,s3, delay='1ms', loss=50)
+
